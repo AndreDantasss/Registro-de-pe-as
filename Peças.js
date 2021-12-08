49bcfd5f445e5c42f8020ccd154144495c28e79e
AndreDantasss <andre.viictor@gmail.com>
2021-12-07 23:54:48 -0300

    adicionando peças

---
+++ Peças.js
@@ -0,0 +1,23 @@
+let ListaDePreco = ["Retrovisor", "Volante", "Pneu"]
+
+if (ListaDePreco. length < 10){
+    console. log("É possivel cadastrar mais peças")
+} else {
+    console. log("Não é possivel cadastrar mais peças")
+}
+
+let peso = 200;
+
+if (peso < 100){
+    console. log("A peça precisa ter no minimo 100 gramas")
+} else {
+    console. log("A peça pode ser registrada") 
+}
+ 
+let NomePeca = "Retrovisor";
+
+if (NomePeca. length >= 3){
+    console. log("A peça pode ser registrada")
+} else {
+    console. log("A peça precisa ter no minimo três caracteres para ser registrada")
+}
